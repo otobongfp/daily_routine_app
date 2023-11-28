2d1f167292143067bafc538c4bb76ac2091e33f6
unknown <otobongfp@gmail.com>
2023-11-28 04:56:01 +0100

    Pushed read and write feature. Also applied same to show

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 # Some Python
 print("A TODO APPLICATION")
-todos = []
 
 while True:
     text = input("Select an action: ADD, SHOW, EDIT, COMPLETED or EXIT application -----> ")
     text = text.strip().lower()
     match text:
         case "add":
-            todo = input("Add a new task --> ") + "\n"
-            todo = todo.title()
+            todo = input("Add a new task ---> ") + "\n"
+            # convert input to lower, then make it titled
+            todo.title()
+            # Read the txt file and then append new text
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
             todos.append(todo)
-            # To write the todos to a file we use the following line below; w means write, r means read
+            file.close()
+            # Add write functionality
             file = open('todos.txt', 'w')
             file.writelines(todos)
+            file.close()
         case "show":
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             for index, item in enumerate(todos):
                 print(f'{index + 1}. {item}')
         case "edit":

c827ed7e68715ae83c9d5e59cdce628c9ba17f2b
unknown <otobongfp@gmail.com>
2023-12-01 18:57:36 +0100

    updates

--- main.py
+++ main.py
@@ -21,20 +21,36 @@ while True:
         case "show":
             file = open('todos.txt', 'r')
             todos = file.readlines()
+            # Below is a list comprehension that strips the extra line
+            # todos = [item.strip('\n') for item in todos]
             file.close()
             for index, item in enumerate(todos):
+                # Also strips the extra line
+                item = item.strip('\n')
                 print(f'{index + 1}. {item}')
         case "edit":
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             edit_value = int(input(f"choose the position number 1 - {len(todos)} to edit: "))
             edit_value = (edit_value - 1)
-            new_todo = input("type edit here: ")
+            new_todo = input("type edit here: ") + "\n"
             new_todo = new_todo.title()
             todos[edit_value] = new_todo
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case "completed":
             print("CHOOSE A TASK THAT HAS BEEN COMPLETED")
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             completed_value = int(input(f"choose the position number 1 - {len(todos)} to delete: "))
             completed_value = completed_value - 1
             todos.pop(completed_value)
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case "exit":
             break
 print("Thanks for using the App")

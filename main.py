9fc6a3c9c540a5cc4e3e8d36bc93bfec1aea2dbe
unknown <otobongfp@gmail.com>
2023-12-01 20:50:47 +0100

    minor update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Some Python
+# Uncomment some lines to try new features
 
 from datetime import datetime
 print("A TODO APPLICATION")
@@ -8,16 +9,19 @@ while True:
     text = text.strip().lower()
     match text:
         case "add":
+            # todo = input("add a new todo ---> ") + "\n"
             mood = input("On scale of 1 - 10 what's your mood?: ") + "\n"
             journal = input("Write some new thoughts ---> ")
             # convert input to lower, then make it titled
             journal = journal.title()
             date = datetime.today().strftime('%Y-%m-%d')
             print(f"today's date is {date}")
-
             with open(f'./journals/{date}.txt', 'w') as file:
                 file.write(f'Your Mood: {mood}\n')
                 file.write(journal)
+            #to add to the todos.txt instead
+            # with open('./todos.txt', 'w') as file:
+            #     file.writelines(todo)
             # Read the txt file and then append new text
             # with open('todos.txt', 'r') as file:
             #     todos = file.readlines()

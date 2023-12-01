b319bdc1382d806c1aab06fde6f30e3c8708fb58
unknown <otobongfp@gmail.com>
2023-12-01 20:05:19 +0100

    Added 'with' context

--- main.py
+++ main.py
@@ -10,36 +10,32 @@ while True:
             # convert input to lower, then make it titled
             todo.title()
             # Read the txt file and then append new text
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            todos.append(todo)
-            file.close()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+                todos.append(todo)
             # Add write functionality
             file = open('todos.txt', 'w')
             file.writelines(todos)
             file.close()
         case "show":
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
             # Below is a list comprehension that strips the extra line
             # todos = [item.strip('\n') for item in todos]
-            file.close()
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
             for index, item in enumerate(todos):
                 # Also strips the extra line
                 item = item.strip('\n')
                 print(f'{index + 1}. {item}')
         case "edit":
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open("./todos.txt", 'r') as file:
+                todos = file.readlines()
             edit_value = int(input(f"choose the position number 1 - {len(todos)} to edit: "))
             edit_value = (edit_value - 1)
             new_todo = input("type edit here: ") + "\n"
             new_todo = new_todo.title()
             todos[edit_value] = new_todo
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+            with open('./todos.txt', 'w') as file:
+                file.writelines(todos)
         case "completed":
             print("CHOOSE A TASK THAT HAS BEEN COMPLETED")
             file = open('todos.txt', 'r')
